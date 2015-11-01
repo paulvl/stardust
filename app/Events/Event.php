e7010362848295f330130c648a7263faaf84adbc
Paul Vidal <paul.vidal.lujan@gmail.com>
2015-11-01 18:36:28 -0500

    first commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace StardustApp\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace StardustApp\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \StardustApp\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \StardustApp\Http\Middleware\VerifyCsrfToken::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \StardustApp\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \StardustApp\Http\Middleware\RedirectIfAuthenticated::class,
+    ];
+}
